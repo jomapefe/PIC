b5dc593597d69e088c69e2f437a432e1057dd852
jomapefe <49369538+jomapefe@users.noreply.github.com>
2021-04-12 15:10:37 +0200

    1

--- PIC16F688/pic16f688.X/build/default/production/main.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/main.o:  \
-	D:/GitHub/PIC/PIC/PIC16F688/pic16f688.X/main.asm  \
-
--- PIC16F688/pic16f688.X/main.asm
+++ PIC16F688/pic16f688.X/main.asm
@@ -13,7 +13,10 @@
  __CONFIG _FOSC_EXTRCCLK  & _WDTE_OFF & _PWRTE_ON & _CP_OFF
  
 ;CODIGO
- 
+ ; declaracion de variables
+REG1		EQU 	0X0c		;asigna nombre a los registros
+REG2		EQU 	0X0d		
+
     ORG 0; Direccion 0 del Programa
 INICIO
     
@@ -29,7 +32,23 @@ INICIO
     BCF	   STATUS,RP0; SELECCIONAMOS EL BANCO 0
    
     
-     
+     ;inicio de secuencia
+PRINCIPAL	BSF		PORTA		;HIGH  a RB4
+			CALL	RETARDO		;llama a retardo
+			BCF		PORTA		;LOW a RB4
+			CALL	RETARDO		;Llama a retardo
+			GOTO	PRINCIPAL	;Retorna a la etiqueta PRINCIPAL
+;retardos
+RETARDO		MOVLW	D'011'		;carga el registro W con 255
+			MOVWF	REG2		;mover el valor de W a REG2
+BUCLE2		MOVWF	REG1		;mover el valor de W a REG1
+BUCLE1		DECFSZ	REG1,F		;Decrementa REG1, salta una linea si es 0
+			GOTO	BUCLE1		;ir a la etiqueta bucle1
+			DECFSZ	REG2,F		;Decrementa REG2, salta una linea si es 0
+			GOTO	BUCLE2		;ir a al etiqueta bucle2
+			RETURN				;retornar al lugar de donde se lo llamo
+			END					;fin de programa
+
     
     
 
@@ -37,7 +56,19 @@ START
     MOVLW  B'11111111'; 8BITS -> W
     MOVWF  PORTA; W -> PORTA. RB0 - RB7
     GOTO   START
-    END
+
+    
+    retardos
+RETARDO		MOVLW	D'011'		;carga el registro W con 255
+			MOVWF	REG2		;mover el valor de W a REG2
+BUCLE2		MOVWF	REG1		;mover el valor de W a REG1
+BUCLE1		DECFSZ	REG1,F		;Decrementa REG1, salta una linea si es 0
+			GOTO	BUCLE1		;ir a la etiqueta bucle1
+			DECFSZ	REG2,F		;Decrementa REG2, salta una linea si es 0
+			GOTO	BUCLE2		;ir a al etiqueta bucle2
+			RETURN				;retornar al lugar de donde se lo llamo
+			END					;fin de programa
+
 
 
 
