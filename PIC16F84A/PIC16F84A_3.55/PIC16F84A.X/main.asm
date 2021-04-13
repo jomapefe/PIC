affe6c866d68151dacadd3f1b2af7636faed2fad
jomapefe <49369538+jomapefe@users.noreply.github.com>
2021-04-13 23:59:23 +0200

    1

--- PIC16F84A/PIC16F84A_3.55/PIC16F84A.X/main.asm
+++ PIC16F84A/PIC16F84A_3.55/PIC16F84A.X/main.asm
@@ -1,17 +1,16 @@
 ;configuracion del compilador
-list p=16f84a
-include <p16f84a.inc>
+    list p=16f84a
+    include <p16f84a.inc>
 ;__CONFIG	_CP_OFF & _WDT_OFF & _PWRTE_ON & _XT_OSC
  __CONFIG _FOSC_EXTRC & _WDTE_OFF & _PWRTE_ON & _CP_OFF
 ; declaracion de variables
 REG1		EQU 	0X0c		;asigna nombre a los registros
 REG2		EQU 	0X0d		
 			ORG 	0			;directiva de origen de programa
-
 ;configuracion de puertos 		
 INICIO		BSF		STATUS,5	;ir al banco (1)
-			CLRF	TRISB		;todo el puerto B es salida
-			BCF		STATUS,5	;ir al banco (0)
+		CLRF	TRISB		;todo el puerto B es salida
+		BCF		STATUS,5	;ir al banco (0)
 
 ;inicio de secuencia
 PRINCIPAL	BSF		PORTB,2		;HIGH  a RB4
@@ -20,7 +19,7 @@ PRINCIPAL	BSF		PORTB,2		;HIGH  a RB4
 			CALL	RETARDO		;Llama a retardo
 			GOTO	PRINCIPAL	;Retorna a la etiqueta PRINCIPAL
 ;retardos
-RETARDO		MOVLW	D'120'		;carga el registro W con 255
+RETARDO		MOVLW	d'110'		;carga el registro W con 255
 			MOVWF	REG2		;mover el valor de W a REG2
 BUCLE2		MOVWF	REG1		;mover el valor de W a REG1
 BUCLE1		DECFSZ	REG1,F		;Decrementa REG1, salta una linea si es 0
