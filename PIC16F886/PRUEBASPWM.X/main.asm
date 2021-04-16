cb3a62f3b103d2e3b1621ea64daff95c0cc47df9
jomapefe <49369538+jomapefe@users.noreply.github.com>
2021-04-16 22:25:30 +0200

    1

--- PIC16F886/PRUEBASPWM.X/main.asm
+++ PIC16F886/PRUEBASPWM.X/main.asm
@@ -11,11 +11,50 @@
 ; __config 0xFFFF
  __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
 
-    ORG 0
-    
-    BSF	STATUS,RP1
-    BSF	STATUS,RP0  ;Banco 3
-    BSF	ANSEL,0	    ;RA0 entrada analogica
-    CLRF    ANSELH  ;PORTB digital
+ 
+M	EQU 0X20
+N	EQU 0X21
+O	EQU 0X22
 
+    ORG 0
+INICIO:
+    BSF	STATUS,5		;Banco 1
+    BSF	TRISA,0		;PORTA0 Entrada	
+    CLRF    TRISC		;PORTC Salida
+    
+    BCF	    STATUS,5	;Banco 0
+ 
+    MOVLW   B'10000001'
+    MOVWF   ADCON0		;10=Fosc/32
+				;0000=AN0
+				;0=A/D conversion completed/not in progress
+				;1 =1 = ADC is enabled
+
+    BSF	STATUS,RP0		;Banco 1
+PRINCIPAL:
+    MOVF    ADRESH,0
+    MOVWF   PORTC
+    CALL    DELAY
+    BSF	    ADCON0,GO_DONE
+    GOTO    PRINCIPAL
+    
+DELAY
+    MOVLW   D'5'
+    MOVWF   O
+TRES
+	MOVLW	D'80'
+	MOVWF	M
+DOS
+	MOVLW	D'250'
+	MOVWF	N
+UNO
+	NOP
+	NOP
+	DECFSZ	N,1
+	GOTO	UNO
+	DECFSZ	M,1
+	GOTO	DOS
+	DECFSZ	O,1
+	GOTO	TRES
+	RETURN
     END
